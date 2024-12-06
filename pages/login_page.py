103c47cd0ab35045dda6bfd8eafd13b6da81253b
Peat <philippmadl1122@gmail.com>
2024-12-06 13:26:41 +0100

    Refactor LoginPage to inherit from BasePage
    This commit refactors the LoginPage class to inherit from the BasePage class, ensuring better code reuse and structure. Methods for entering text and clicking buttons now utilize inherited methods from BasePage, simplifying the implementation. The redundant lines of code for handling WebDriver wait conditions have been removed, resulting in a cleaner and more maintainable codebase.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,58 +1,33 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
 import config
 
-
-class LoginPage:
+class LoginPage(BasePage):
     URL = config.LOGIN_URL
 
-    def __init__(self, driver: WebDriver):
-        self.driver = driver
-
-        self.username_input = (By.ID, "username")
-        self.password_input = (By.ID, "password")
-        self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
+    def __init__(self, driver):
+        super().__init__(driver)  # Konstruktor der Basisklasse aufrufen
+        #self.username_input = (By.ID, "username")
+        #self.password_input = (By.ID, "password")
+        #self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
         self.error_message = (By.CSS_SELECTOR, "div#error-message-container p")
-        self.logout_button = (By.ID, "logout_button")
+
 
     def load(self):
         self.driver.get(self.URL)
 
     def enter_username(self, username: str):
-        username_field = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.username_input)
-        )
-        username_field.clear()
-        username_field.send_keys(username)
+        self.enter_text(self.username_input, username)
 
     def enter_password(self, password: str):
-        password_field = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.password_input)
-        )
-        password_field.clear()
-        password_field.send_keys(password)
+        self.enter_text(self.password_input, password)
 
     def click_login(self):
-        login_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.login_button)
-        )
-        login_button.click()
+        self.wait_and_click(self.login_button)
 
     def get_error_message(self):
-        error_element = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(self.error_message)
-        )
-        return error_element.text
+        return self.wait_for_element(self.error_message).text
 
-    def login(self, username: str, password: str):
-        self.enter_username(username)
-        self.enter_password(password)
-        self.click_login()
 
-    def logout(self):
-        logout_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.logout_button)
-        )
-        logout_button.click()
+
+
